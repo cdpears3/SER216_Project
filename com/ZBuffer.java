3eaf14214dfa04a0e06448e1219ed90b4ded1a09
cdpears3 <cdpears3@asu.edu>
2017-05-02 00:27:18 -0700

    Update ZBuffer.java

--- com/ZBuffer.java
+++ com/ZBuffer.java
@@ -13,29 +13,61 @@ public class ZBuffer{
 		public double p_y=0;
 		public double p_z=0;
 
-		
+		/**
+		 * Gets the integer value of this color.
+		 * @return the integer value of this color
+		 */
 		public int getRgbColor() {
 			return rgbColor;
 		}
+		
+		/**
+		 * Sets this color to the given integer value
+		 * @param rgbColor the new color
+		 */
 		public void setRgbColor(int rgbColor) {
 			this.rgbColor = rgbColor;
 		}
+		
+		/**
+		 * Return the z value of this ZBuffer.
+		 * @return this z
+		 */
 		public double getZ() {
 			return z;
 		}
+		
+		/**
+		 * Sets the z value of this ZBuffer.
+		 * @param z the new value
+		 */
 		public void setZ(double z) {
 			this.z = z;
 		}
 		
+		/**
+		 * Constructs a ZBuffer with given parameters.
+		 * @param rgbColor the color of this buffer
+		 * @param z	the z value of this buffer
+		 */
 		public ZBuffer(int rgbColor, double z) {
 			super();
 			this.rgbColor = rgbColor;
 			this.z = z;
 		}
+		
+		/**
+		 * Default ZBuffer constructor.
+		 */
 		public ZBuffer() {
 			super();
 		}
 		
+		/**
+		 * Returns the color of the given hex string.
+		 * @param col	the hex value of the color
+		 * @return	the color of the hex value
+		 */
 		public static Color  fromHexToColor(String col){
 
 
@@ -49,6 +81,11 @@ public class ZBuffer{
 			return color;
 		}
 
+		/**
+		 * Returns the hex value of a color as a string.
+		 * @param col	the color being converting to hex
+		 * @return	the hex value of the color
+		 */
 		public static String fromColorToHex(Color col){
 
 			String exe="";
@@ -61,7 +98,11 @@ public class ZBuffer{
 
 		}
 
-
+		/**
+		 * Prepends zero's to the hex value to fill it out.
+		 * @param hexString	the hex value being filled
+		 * @return	the filled hex value
+		 */
 		public static String addZeros(String hexString) {
 			
 			if(hexString.length()==1)
@@ -70,7 +111,13 @@ public class ZBuffer{
 				return hexString;
 		}
 		
-		
+		/**
+		 * Update the color.
+		 * @param xs the x position for the new color
+		 * @param ys the y position for the new color
+		 * @param zs the z position for the new color
+		 * @param rgbColor	the new color
+		 */
 		public void update(double xs,double ys,double zs, int rgbColor) {
 			
 			
@@ -87,12 +134,24 @@ public class ZBuffer{
 
 		}
 
+		/**
+		 * Checks if the color has been changed.
+		 * @param ys	
+		 * @return	true if the color hasn't been changed, false otherwise
+		 */
 		public boolean isToUpdate(double ys){
 
 
 			return getZ()==0 ||  getZ()>ys;
 		}	
-
+		
+		/**
+		 * Sets the color of the given position.
+		 * @param xs	the x position
+		 * @param ys	the y position
+		 * @param zs	the z position
+		 * @param rgbColor	the new color
+		 */
 		public void set(double xs,double ys,double zs, int rgbColor) {
 
 			setZ(ys);
